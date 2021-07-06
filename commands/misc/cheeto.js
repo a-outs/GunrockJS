1ca660f4415d8f89c7abc8f354e5e123d1a66f34
timstewartj <timstewartj@gmail.com>
2021-07-05 19:48:03 -0700

    cheeto command

---
+++ commands/misc/cheeto.js
@@ -0,0 +1,31 @@
+const { cheeto_pics } = require("../../cheeto.json");
+const { MessageButton } = require("discord.js");
+
+module.exports = {
+  name: "cheeto",
+  description: "Returns random picture of Cheeto the cat",
+  hasCommand: true,
+  hasSlash: true,
+  hasButton: true,
+  helpEntry: true,
+  execute(message, args) {
+    message.channel.send(getCheetoPic());
+  },
+  async slash_execute(interaction) {
+    await interaction.reply(getCheetoPic());
+  },
+  async button_execute(interaction) {
+    await interaction.update(getCheetoPic());
+  },
+};
+
+const getCheetoPic = () => {
+  const button = new MessageButton()
+    .setCustomId("cheeto")
+    .setLabel("Get new pic!")
+    .setStyle("SECONDARY");
+  return {
+    content: cheeto_pics[Math.floor(Math.random() * cheeto_pics.length)][0],
+    components: [[button]],
+  };
+};
