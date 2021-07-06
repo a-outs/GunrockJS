ead837cbd356f5a5bcc01feb04040f99d357b163
timstewartj <timstewartj@gmail.com>
2021-07-05 19:48:28 -0700

    finish setting up slash commands

---
+++ commands/testing/deploy.js
@@ -0,0 +1,32 @@
+const { slash_commands } = require("../../slash_commands.json");
+
+module.exports = {
+  name: "deploy",
+  description: "Deploy",
+  hasCommand: true,
+  hasSlash: false,
+  hasButton: false,
+  helpEntry: false,
+  async execute(message, args) {
+    const timmieID = "372696487290863618";
+    if (message.author.id === timmieID) {
+      if (args[0] === "global") {
+        // to update slash commands GLOBALLY run the following line
+        await message.client.application?.commands.set(slash_commands);
+        message.reply(
+          "Slash commands deployed GLOBALLY, may take up to an hour to register!"
+        );
+      } else {
+        // updates slash commands in guild
+        await message.client.guilds.cache
+          .get(message.guild.id)
+          .commands.set(slash_commands);
+        message.reply(
+          "Slash commands successfully deployed in GUILD " + message.guild.name
+        );
+      }
+
+      return;
+    }
+  },
+};
