5b9c734b181aaddbec2e3650dc486831aad64cf4
timstewartj <timstewartj@gmail.com>
2021-07-12 18:30:49 -0700

    boomer command

---
+++ commands/pings/boomer.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: "boomer",
+  description: "Calls someone a boomer",
+  hasCommand: true,
+  hasSlash: true,
+  hasButton: false,
+  helpEntry: true,
+  execute(message, args) {
+    if (args.length < 1) {
+      return message.reply("Error! Please provide a user to call boomer!");
+    }
+    return message.channel.send(args[0] + " okay boomer");
+  },
+  async slash_execute(interaction) {
+    const { value: name } = interaction.options.get("name");
+    await interaction.reply(`<@${name}> okay boomer`);
+  },
+};
