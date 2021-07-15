583c164950b7843a51dd8b72d06cc0c5cec4b29a
timstewartj <timstewartj@gmail.com>
2021-07-14 22:42:06 -0700

    cow command

---
+++ commands/pings/cow.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "cow",
+  description: "Detects cow percentage in a @user",
+  hasCommand: true,
+  hasSlash: true,
+  hasButton: false,
+  helpEntry: true,
+  cooldown: 60,
+  execute(message, args) {
+    if (args.length < 1) {
+      return message.reply("Error! Please provide a user to rate!");
+    }
+    return message.channel.send(cowR8Machine(args[0]));
+  },
+  async slash_execute(interaction) {
+    const { value: name } = interaction.options.get("name");
+    await interaction.reply(cowR8Machine(`<@${name}>`));
+  },
+};
+
+const cowR8Machine = (name) => {
+  return {
+    embeds: [
+      new MessageEmbed()
+        .setTitle("cow r8 machine")
+        .setDescription(`${name} is ${Math.floor(Math.random() * 100)}% cow`)
+        .setColor("0xffbf00"),
+    ],
+  };
+};
